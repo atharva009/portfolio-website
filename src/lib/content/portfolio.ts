75db1f54adaf85318564c37b577348c52218749d
atharva009 <atharvahankare@gmail.com>
2025-01-19 16:21:58 -0500

    feat: add portfolio content

---
+++ src/lib/content/portfolio.ts
@@ -0,0 +1,38 @@
+/*
+ * =========================
+ * AUTHOR INFO
+ * =========================
+ */
+
+import { StringKeyValueType } from '../types';
+
+export const socialLinks: StringKeyValueType = {
+  github: 'https://github.com/atharva009',
+  linkedin: 'https://www.linkedin.com/in/atharva-hankare/',
+  twitter: 'https://x.com/AtharvaHankare',
+  instagram: 'https://www.instagram.com/atharvah09',
+};
+
+export const author = {
+  name: 'A H',
+  email: 'hankare.a@northeastern.edu',
+};
+
+export const seoData = {
+  title: 'Atharva Hankare',
+  description:
+    'Atharva Hankare is a front-end developer who specializes in building (and occasionally designing) exceptional visual interfaces.',
+  author: author.name,
+  image:
+    'https://user-images.githubusercontent.com/68834718/217467445-0b0d2d63-f8ad-4702-8474-814eb2cbb3be.png',
+  url: 'https://atharvahankare.netlify.app',
+  keywords: [
+    'Atharva',
+    'Atharva Hankare',
+    '@atharvah09',
+    'atharvah09',
+    'Portfolio',
+    'Atharva Portfolio ',
+    'Atharva Hankare Portfolio',
+  ],
+};
