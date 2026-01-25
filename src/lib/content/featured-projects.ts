52aef7db9ee9970dc36d80cea00c84b2158ef317
atharva009 <atharvahankare@gmail.com>
2026-01-24 21:02:29 -0500

    update link to project

--- src/lib/content/featured-projects.ts
+++ src/lib/content/featured-projects.ts
@@ -10,7 +10,7 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
       description: 'A Talent Management and Employee Engagement Platform.',
       tasks:
         "Conceptualized, designed, and developed the TalentCove platform to address challenges in talent management and employee engagement. The project involved building an intuitive frontend interface, developing backend services, and deploying the application on a scalable cloud infrastructure.",
-      url: 'https://atharva-hankare.notion.site/TalentCove-1b371b06b30a81978238c386ae5b3160',
+      url: 'https://www.notion.so/atharva-hankare/TalentCove-1b371b06b30a81978238c386ae5b3160',
       img: '/talentcove.png',
       tags: [
         'React',
@@ -26,7 +26,7 @@ const featuredProjectsSection: FeaturedProjectsSectionType = {
       description: 'EverAfter is your AI-powered date planning companion that helps couples create, cherish, and discover unforgettable romantic experiences.',
       tasks:
         "Conceptualized, designed, and developed EverAfter—a personalized date planning platform enabling couples to log memories, revisit past experiences, and receive AI-powered itinerary suggestions. Built an intuitive, emotionally engaging interface focused on enhancing relationship moments through thoughtful planning and reflection.",
-      url: 'https://atharva-hankare.notion.site/EverAfter-1b371b06b30a8045be13e54884cb78ec',
+      url: 'https://www.notion.so/atharva-hankare/EverAfter-1b371b06b30a8045be13e54884cb78ec',
       img: '/everafter.png',
       tags: [
         'Java',
